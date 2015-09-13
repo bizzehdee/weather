27ac9cecce6fa7afe40ecde0fbe876cb096d6902
Darren Horrocks <killallthehumans@gmail.com>
2015-09-13 22:58:18 +0000

    directory layout plus basic script that checks the layout and parses arguments

---
+++ weather.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python
+
+################################################################
+#
+# This project is distributed under the MIT licence, see LICENCE
+#
+################################################################
+
+import os
+import datetime
+import time
+import random
+import sys
+
+if not os.path.exists("weather"):
+	raise Exception("weather dir missing")
+
+
