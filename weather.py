12f3f1ef75b9378acf5c751c9406e4437182b888
Darren Horrocks <killallthehumans@gmail.com>
2015-09-14 22:16:29 +0000

    date range looping implemented

--- weather.py
+++ weather.py
@@ -12,7 +12,32 @@ import time
 import random
 import sys
 
+WEATHER_CSV_URL = "http://www.wunderground.com/history/airport/%s/%04d/%02d/%02d/DailyHistory.html?format=1"
+
 if not os.path.exists("weather"):
 	raise Exception("weather dir missing")
 
+argc = len(sys.argv)
 
+dateFrom = time.localtime(time.time())
+dateTo = time.localtime(time.time())
+airport_file = ""
+airportCodes = []
+
+for x in range(0, argc):
+	if sys.argv[x] == "-from":
+		dateFromStr = sys.argv[x+1]
+		dateFrom = time.strptime(dateFromStr, "%Y-%m-%d")
+	elif sys.argv[x] == "-to":
+		dateToStr = sys.argv[x+1]
+		dateTo = time.strptime(dateToStr, "%Y-%m-%d")
+	elif sys.argv[x] == "-airport_file":
+		airport_file = sys.argv[x+1]
+	elif sys.argv[x] == "-airport":
+		airportCodes.append(sys.argv[x+1])
+
+if os.path.exists(airport_file):
+	print(airport)
+
+for airportCode in airportCodes:
+	print(airportCode)
